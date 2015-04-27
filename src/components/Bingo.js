94fd060d6a0a3542f35f9215b0e16ed7ed36b4bc
J2P <jjp5023@gmail.com>
2015-04-28 00:37:54 +0900

    랜덤숫자 생성 함수 내부로 이동

--- src/components/Bingo.js
+++ src/components/Bingo.js
@@ -1,20 +1,6 @@
 var React = require('react');
 var Board = require('./Board.js');
 
-function getRandomNumbers() {
-	var board = [];
-	var numbers = [
-		1, 2, 3, 4, 5, 
-		6, 7, 8, 9, 10,
-		11, 12, 13, 14, 15,
-		16, 17, 18, 19, 20,
-		21, 22, 23, 24, 25
-	];
-	for (var i = 0; i < 25; i++) {
-		board.push({value: numbers.splice(Math.floor(Math.random() * numbers.length), 1)});	
-	}
-	return board
-}
 
 var Bingo = React.createClass({
 	getInitialState: function() {
@@ -35,6 +21,24 @@ var Bingo = React.createClass({
 		);
 	}
 
+	_getRandomNumbers: function() {
+		var board = [];
+		var numbers = [
+			1, 2, 3, 4, 5, 
+			6, 7, 8, 9, 10,
+			11, 12, 13, 14, 15,
+			16, 17, 18, 19, 20,
+			21, 22, 23, 24, 25
+		];
+		for (var i = 0; i < 25; i++) {
+			board.push({
+				id: i + 1,
+				selected: false,
+				value: numbers.splice(Math.floor(Math.random() * numbers.length), 1)
+			});	
+		}
+		return board
+	}
 });
 
 module.exports = Bingo;
