a7ad477fd6c31444851dd48e7eea0ac68e9db940
J2P <jjp5023@gmail.com>
2015-04-27 23:55:50 +0900

    Close #3 cell 선택하면 동그라미 표시 되도록 구현

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,9 +1,25 @@
-var React = require('react');
+var React = require('react/addons');
 
 var Cell = React.createClass({
+	getInitialState: function() {
+		return {
+			selected: false 
+		};
+	},
+	handleClick: function() {
+		this.setState({ selected: true });
+	},
 	render: function() {
+		var classes = React.addons.classSet({
+				'cell': true,
+        'selected': this.state.selected
+    });
+
 		return (
-			<div className="cell">{this.props.value}</div>
+			<div className={classes} onClick={this.handleClick}>
+				<div className="circle"></div>
+				{this.props.value}
+			</div>
 		);
 	}
 });
