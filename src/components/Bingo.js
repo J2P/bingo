8eb8980d163042ad6b46c8c9c997a6b659d013c2
J2P <jjp5023@gmail.com>
2015-05-01 23:39:16 +0900

    bingo 게임 데이터를 서버에서 관리하고 보내주도록 수정

--- src/components/Bingo.js
+++ src/components/Bingo.js
@@ -1,38 +1,47 @@
-var socket = io();
+var socket = require('socket.io-client')('http://localhost:3000');
 var React = require('react');
 var Cell = require('./Cell.js');
 
 var Bingo = React.createClass({
-	setInitial: function(data) {
-		var initData = { board: data.cells };
-		initData[data.color] = true;
-		this.setState(initData);
-	},
-
 	getInitialState: function() {
-		socket.on('send:number', this.selectNumber);
 		socket.on('init', this.setInitial);
+		socket.on('change:number', this.changeNumber);
 
 		return {
 			blue: false,
-	      	green: false,
-	      	red: false,
-	      	orange: false,
-	      	purple: false,
-			board: []
+    	green: false,
+    	red: false,
+    	orange: false,
+    	purple: false,
+			board: [],
+			lines: []
 		}
 	},
 
 	componentDidMount: function() {
-		socket.emit('join', {user:'user' + Math.random().toString(36).substring(7)});
+		this.user = 'user_' + Math.random().toString(36).substring(7);
+		socket.emit('join', { 
+			user : this.user 
+		});
 	},
 
 	handleClick: function(cell) {
-		var number = cell.props.value;
-		socket.emit('send:number', {number: number});
-		this.setState({board: this.getSelectNumber(number)});
+		socket.emit('send:number', {
+			user: this.user,
+			number: cell.props.value
+		});
 	},
 	
+	setInitial: function(data) {
+		var initData = { board: data.board };
+		initData[data.color] = true;
+		this.setState(initData);
+	},
+
+	changeNumber: function(data) {
+		this.setState({board: data.users[this.user]['board']});
+	},
+
 	render: function() {
 		var self = this;
 		var cells = this.state.board.map(function(cell) {
@@ -62,54 +71,6 @@ var Bingo = React.createClass({
 				</div>
 			</div>
 		);
-	},
-
-	selectNumber: function(data) {
-		var board = this.getSelectNumber(data.number);
-		this.setState({board: board});
-	},
-
-	getSelectNumber: function(number) {
-		var cells = this.state.board;
-		var board = [];
-
-		for (var i = 0; i < cells.length; i++) {
-			if (cells[i].value == number) {
-				board.push({
-					id: cells[i].id,
-					selected: true,
-					value: cells[i].value
-				});
-			} else {
-				board.push({
-					id: cells[i].id,
-					selected: cells[i].selected,
-					value: cells[i].value
-				});
-			}
-		}
-
-		return board
-	},
-
-	getRandomNumbers: function() {
-		var cells = [];
-		var numbers = [
-			1, 2, 3, 4, 5, 
-			6, 7, 8, 9, 10,
-			11, 12, 13, 14, 15,
-			16, 17, 18, 19, 20,
-			21, 22, 23, 24, 25
-		];
-
-		for (var i = 0; i < 25; i++) {
-			cells.push({
-				id: i + 1,
-				selected: false,
-				value: numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0]
-			});	
-		}
-		return cells
 	}
 });
 
